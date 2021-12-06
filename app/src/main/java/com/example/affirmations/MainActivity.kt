ec14880e73c2355b430252ee68cb06f316e5dd4a
Your Name <you@example.com>
2021-12-06 14:40:51 +0700

    Add ItemAdapter.kt (ongoing)

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -9,9 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView : TextView = findViewById(R.id.textview)
-        textView.text = Datasource().loadAffirmation().size.toString()
-    }
 
+    }
 
 }
